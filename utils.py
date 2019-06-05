c87d700df7b9d83e7a5eea7f33d70d9df8042c5c
leftthomas <leftthomas@qq.com>
2019-06-05 10:10:46 +0800

    remove google landmark download code

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
-import torch.nn.functional as F
-from torch import nn
+import os
+
+import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
@@ -24,3 +25,36 @@ def load_data(batch_size=64):
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
     return train_loader, test_loader
 
+
+DB_dir = 'database'
+DB_csv = 'data.csv'
+
+
+class Database(object):
+
+    def __init__(self):
+        self._gen_csv()
+        self.data = pd.read_csv(DB_csv)
+        self.classes = set(self.data["cls"])
+
+    def _gen_csv(self):
+        if os.path.exists(DB_csv):
+            return
+        with open(DB_csv, 'w', encoding='UTF-8') as f:
+            f.write("img,cls")
+            for root, _, files in os.walk(DB_dir, topdown=False):
+                cls = root.split('/')[-1]
+                for name in files:
+                    if not name.endswith('.jpg'):
+                        continue
+                    img = os.path.join(root, name)
+                    f.write("\n{},{}".format(img, cls))
+
+    def __len__(self):
+        return len(self.data)
+
+    def get_class(self):
+        return self.classes
+
+    def get_data(self):
+        return self.data
