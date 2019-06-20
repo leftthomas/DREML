20e92cac9d60c32917e78d840eb3fc4b7a909c91
leftthomas <leftthomas@qq.com>
2019-06-20 14:30:31 +0800

    finish cars

--- data_utils.py
+++ data_utils.py
@@ -23,26 +23,26 @@ def read_txt(path):
 
 
 def process_car_data(data_path):
-    classes, trains, tests = {}, {}, {}
+    train_images, test_images, class_names = {}, {}, {}
     annos_mat = loadmat('{}/cars_annos.mat'.format(data_path))
-    class_names, annotations = annos_mat['class_names'][0], annos_mat['annotations'][0]
-    for index, class_name in enumerate(class_names):
-        classes[index + 1] = str(class_name[0])
-    write_json(classes, '{}/{}'.format(data_path, class_json))
+    classes, annotations = annos_mat['class_names'][0], annos_mat['annotations'][0]
+    for index, class_name in enumerate(classes):
+        class_names[index + 1] = str(class_name[0])
+    write_json(class_names, '{}/{}'.format(data_path, class_name_json))
 
     for img in annotations:
-        img_name, img_label, flag = str(img[0][0]), str(img[-2][0][0]), int(img[-1][0][0])
-        if flag == 0:
-            trains['{}/{}'.format(data_path, img_name)] = img_label
+        img_name, img_label = str(img[0][0]), str(img[-2][0][0])
+        if int(img_label) < 99:
+            train_images['{}/{}'.format(data_path, img_name)] = img_label
         else:
-            tests['{}/{}'.format(data_path, img_name)] = img_label
-    write_json(trains, '{}/{}'.format(data_path, train_json))
-    write_json(tests, '{}/{}'.format(data_path, test_json))
+            test_images['{}/{}'.format(data_path, img_name)] = img_label
+    write_json(train_images, '{}/{}'.format(data_path, train_image_json))
+    write_json(test_images, '{}/{}'.format(data_path, test_image_json))
 
 
 def process_cub_data(data_path):
     classes = read_txt('{}/classes.txt'.format(data_path))
-    write_json(classes, '{}/{}'.format(data_path, class_json))
+    write_json(classes, '{}/{}'.format(data_path, class_name_json))
     images = read_txt('{}/images.txt'.format(data_path))
     splits = read_txt('{}/train_test_split.txt'.format(data_path))
     labels = read_txt('{}/image_class_labels.txt'.format(data_path))
@@ -52,8 +52,8 @@ def process_cub_data(data_path):
             trains['{}/{}'.format(data_path, img_name)] = labels[str(index + 1)]
         else:
             tests['{}/{}'.format(data_path, img_name)] = labels[str(index + 1)]
-    write_json(trains, '{}/{}'.format(data_path, train_json))
-    write_json(tests, '{}/{}'.format(data_path, test_json))
+    write_json(trains, '{}/{}'.format(data_path, train_image_json))
+    write_json(tests, '{}/{}'.format(data_path, test_image_json))
 
 
 def process_sop_data(data_path):
@@ -71,13 +71,13 @@ def process_sop_data(data_path):
 
     for index, class_name in enumerate(sorted(class_names)):
         classes[index + 1] = class_name
-    write_json(classes, '{}/{}'.format(data_path, class_json))
-    write_json(trains, '{}/{}'.format(data_path, train_json))
-    write_json(tests, '{}/{}'.format(data_path, test_json))
+    write_json(classes, '{}/{}'.format(data_path, class_name_json))
+    write_json(trains, '{}/{}'.format(data_path, train_image_json))
+    write_json(tests, '{}/{}'.format(data_path, test_image_json))
 
 
 if __name__ == '__main__':
-    train_json, test_json, class_json = 'train.json', 'test.json', 'class.json'
+    train_image_json, test_image_json, class_name_json = 'train_images.json', 'test_images.json', 'class_names.json'
     process_car_data('data/cars')
     process_cub_data('data/cub')
     process_sop_data('data/sop')
