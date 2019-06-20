fdfa0763773855499fc1885d6607a93273f65d48
leftthomas <leftthomas@qq.com>
2019-06-20 17:43:44 +0800

    revert dataset

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -17,10 +19,10 @@ def get_transform(data_name, data_type):
     normalize = transforms.Normalize(rgb_mean[data_name], rgb_std[data_name])
     if data_type == 'train':
         transform = transforms.Compose(
-            [transforms.Resize(256), transforms.RandomCrop(256), transforms.ToTensor(), normalize])
+            [transforms.Resize(224), transforms.RandomCrop(224), transforms.ToTensor(), normalize])
     else:
         transform = transforms.Compose(
-            [transforms.Resize(256), transforms.CenterCrop(256), transforms.ToTensor(), normalize])
+            [transforms.Resize(224), transforms.CenterCrop(224), transforms.ToTensor(), normalize])
     return transform
 
 
@@ -28,12 +30,18 @@ class DiverseLoss(nn.Module):
     def __init__(self):
         super(DiverseLoss, self).__init__()
 
-    def forward(self, output, target):
-        batch_size = output.size(0)
-        # loss
-        dist = -F.log_softmax(output, dim=-1)
-        loss = dist[torch.arange(batch_size), target].mean()
-        return loss
+    def forward(self, output, positives, negatives, model):
+        output = output.unsqueeze(dim=1)
+        p_samples = positives.view(-1, *positives.size()[2:])
+        n_samples = negatives.view(-1, *negatives.size()[2:])
+        p_out, n_out = model(p_samples), model(n_samples)
+        p_out = p_out.view(output.size(0), -1, p_out.size(-1))
+        n_out = n_out.view(output.size(0), -1, n_out.size(-1))
+        p_loss = torch.abs(output.norm(dim=-1) - p_out.norm(dim=-1)).mean(dim=-1)
+        n_loss = torch.abs(output.norm(dim=-1) - n_out.norm(dim=-1)).mean(dim=-1).clamp(min=1e-8).pow(-1)
+        p_direction_loss = (1 + F.cosine_similarity(output, p_out, dim=-1)).mean(dim=-1)
+        loss = p_loss + n_loss + p_direction_loss
+        return loss.mean()
 
 
 class RecallMeter(meter.Meter):
@@ -68,21 +76,43 @@ class RecallMeter(meter.Meter):
 
 
 class RetrievalDataset(Dataset):
-    def __init__(self, data_name, data_type='train'):
+    def __init__(self, data_name, data_type='train', k=10):
 
-        data = read_json('data/{}/{}_images.json'.format(data_name, data_type))
+        if data_type == 'val':
+            data = read_json('data/{}/train_images.json'.format(data_name))
+        else:
+            data = read_json('data/{}/{}_images.json'.format(data_name, data_type))
         self.transform = get_transform(data_name, data_type)
+        self.data_type = data_type
         self.images, self.labels = list(data.keys()), list(data.values())
         self.classes, labels = sorted(set(data.values())), {}
         for index, label in enumerate(self.classes):
             labels[label] = index
         for index, label in enumerate(self.labels):
             self.labels[index] = labels[label]
+        self.k, self.indexes = k, set(range(len(self.images)))
 
     def __getitem__(self, index):
         img_path, label = self.images[index], self.labels[index]
         img = self.transform(Image.open(img_path).convert('RGB'))
-        return img, label
+
+        if self.data_type == 'train':
+            positive_index = set(np.where(np.array(self.labels) == label)[0].tolist())
+            negative_index = self.indexes - positive_index
+            # make sure the search database don't contain itself
+            positive_database = list(positive_index - {index})
+            negative_database = list(negative_index)
+            # choose k samples
+            positive_database = random.choices(positive_database, k=self.k)
+            negative_database = random.choices(negative_database, k=self.k)
+            positives, negatives = [], []
+            for i in range(self.k):
+                positives.append(self.transform(Image.open(self.images[positive_database[i]]).convert('RGB')))
+                negatives.append(self.transform(Image.open(self.images[negative_database[i]]).convert('RGB')))
+            positives, negatives = torch.stack(positives), torch.stack(negatives)
+            return img, positives, negatives
+        else:
+            return img, label, index
 
     def __len__(self):
         return len(self.images)
