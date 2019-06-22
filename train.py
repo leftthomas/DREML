5803fa86b903d6cb017fe0b1cfb4fe1efc78448b
leftthomas <leftthomas@qq.com>
2019-06-22 17:22:47 +0800

    revert branch

---
+++ train.py
@@ -0,0 +1,129 @@
+import argparse
+
+import pandas as pd
+import torch
+import torch.optim as optim
+import torchnet as tnt
+from torch.utils.data import DataLoader
+from torchnet.logger import VisdomPlotLogger
+from tqdm import tqdm
+
+import utils
+from model import Model
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train Image Retrieval Model')
+    parser.add_argument('--data_name', default='cars', type=str, choices=['cars', 'cub', 'sop'], help='dataset name')
+    parser.add_argument('--recalls', default='1,2,4,8', type=str, help='selected recall')
+    parser.add_argument('--batch_size', default=32, type=int, help='training batch size')
+    parser.add_argument('--num_epochs', default=100, type=int, help='train epoch number')
+
+    opt = parser.parse_args()
+
+    DATA_NAME, RECALLS, BATCH_SIZE, NUM_EPOCH = opt.data_name, opt.recalls, opt.batch_size, opt.num_epochs
+    recall_ids = [int(k) for k in RECALLS.split(',')]
+    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    results = {'train_loss': []}
+    for k in recall_ids:
+        results['train_recall_{}'.format(k)], results['test_recall_{}'.format(k)] = [], []
+
+    train_set = utils.RetrievalDataset(DATA_NAME, data_type='train')
+    val_set = utils.RetrievalDataset(DATA_NAME, data_type='val')
+    test_set = utils.RetrievalDataset(DATA_NAME, data_type='test')
+    train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, num_workers=16, shuffle=True)
+    val_loader = DataLoader(val_set, batch_size=BATCH_SIZE, num_workers=16, shuffle=False)
+    test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, num_workers=16, shuffle=False)
+
+    # load data to memory
+    print('load data to memory, it may take a while')
+    val_database, val_labels, test_database, test_labels = [], [], [], []
+    for img, label, index in val_loader:
+        val_database.append(img)
+        val_labels.append(label)
+    val_labels = torch.cat(val_labels)
+    for img, label, index in test_loader:
+        test_database.append(img)
+        test_labels.append(label)
+    test_labels = torch.cat(test_labels)
+
+    model = Model().to(DEVICE)
+    loss_criterion = utils.DiverseLoss()
+    optimizer = optim.Adam(params=model.parameters())
+    print("# parameters:", sum(param.numel() for param in model.parameters()))
+
+    meter_loss = tnt.meter.AverageValueMeter()
+    meter_recall = utils.RecallMeter(topk=recall_ids)
+    loss_logger = VisdomPlotLogger('line', env=DATA_NAME, opts={'title': 'Loss'})
+    recall_logger = VisdomPlotLogger('line', env=DATA_NAME, opts={'title': 'Recall'})
+
+    for epoch in range(1, NUM_EPOCH + 1):
+        # train loop
+        model.train()
+        train_progress, num_data = tqdm(train_loader), 0
+        for img, positives, negatives in train_progress:
+            num_data += img.size(0)
+            img, positives, negatives = img.to(DEVICE), positives.to(DEVICE), negatives.to(DEVICE)
+            optimizer.zero_grad()
+            out = model(img)
+            loss = loss_criterion(out, positives, negatives, model)
+            loss.backward()
+            optimizer.step()
+            meter_loss.add(loss.item())
+            train_progress.set_description('Train Epoch: {}---{}/{} Loss: {:.2f}'.format(
+                epoch, num_data, len(train_set), meter_loss.value()[0]))
+        loss_logger.log(epoch, meter_loss.value()[0], name='train')
+        results['train_loss'].append(meter_loss.value()[0])
+        meter_loss.reset()
+
+        # test loop
+        with torch.no_grad():
+            model.eval()
+            val_features = []
+            for data in val_database:
+                data = data.to(DEVICE)
+                val_feature = model(data)
+                val_features.append(val_feature.detach().cpu())
+            val_features = torch.cat(val_features)
+            # compute recall for train data
+            val_progress, num_data = tqdm(val_loader), 0
+            for img, label, index in val_progress:
+                num_data += img.size(0)
+                img = img.to(DEVICE)
+                out = model(img).detach().cpu()
+                meter_recall.add(out, index, label, val_features, val_labels)
+                desc = 'Val Epoch: {}---{}/{}'.format(epoch, num_data, len(val_set))
+                for i, k in enumerate(recall_ids):
+                    desc += ' Recall@{}: {:.2f}%'.format(k, meter_recall.value()[i])
+                val_progress.set_description(desc)
+            for i, k in enumerate(recall_ids):
+                recall_logger.log(epoch, meter_recall.value()[i], name='train_recall_{}'.format(k))
+                results['train_recall_{}'.format(k)].append(meter_recall.value()[i])
+            meter_recall.reset()
+
+            test_features = []
+            for data in test_database:
+                data = data.to(DEVICE)
+                test_feature = model(data)
+                test_features.append(test_feature.detach().cpu())
+            test_features = torch.cat(test_features)
+            # compute recall for test data
+            test_progress, num_data = tqdm(test_loader), 0
+            for img, label, index in test_progress:
+                num_data += img.size(0)
+                img = img.to(DEVICE)
+                out = model(img).detach().cpu()
+                meter_recall.add(out, index, label, test_features, test_labels)
+                desc = 'Test Epoch: {}---{}/{}'.format(epoch, num_data, len(test_set))
+                for i, k in enumerate(recall_ids):
+                    desc += ' Recall@{}: {:.2f}%'.format(k, meter_recall.value()[i])
+                test_progress.set_description(desc)
+            for i, k in enumerate(recall_ids):
+                recall_logger.log(epoch, meter_recall.value()[i], name='test_recall_{}'.format(k))
+                results['test_recall_{}'.format(k)].append(meter_recall.value()[i])
+            meter_recall.reset()
+
+        # save model
+        torch.save(model.state_dict(), 'epochs/{}_{}.pth'.format(DATA_NAME, epoch))
+        # save statistics
+        data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
+        data_frame.to_csv('statistics/{}_results.csv'.format(DATA_NAME), index_label='epoch')
---
+++ utils.py
@@ -0,0 +1,131 @@
+import random
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from PIL import Image
+from torch.utils.data import Dataset
+from torchnet.meter import meter
+from torchvision import transforms
+
+from data_utils import read_json
+
+rgb_mean = {'cars': [0.4853, 0.4965, 0.4295], 'cub': [0.4707, 0.4601, 0.4549], 'sop': [0.5807, 0.5396, 0.5044]}
+rgb_std = {'cars': [0.2237, 0.2193, 0.2568], 'cub': [0.2767, 0.2760, 0.2850], 'sop': [0.2901, 0.2974, 0.3095]}
+
+
+def get_transform(data_name, data_type):
+    normalize = transforms.Normalize(rgb_mean[data_name], rgb_std[data_name])
+    if data_type == 'train':
+        transform = transforms.Compose(
+            [transforms.Resize(224), transforms.RandomCrop(224), transforms.ToTensor(), normalize])
+    else:
+        transform = transforms.Compose(
+            [transforms.Resize(224), transforms.CenterCrop(224), transforms.ToTensor(), normalize])
+    return transform
+
+
+class DiverseLoss(nn.Module):
+    def __init__(self):
+        super(DiverseLoss, self).__init__()
+
+    def forward(self, output, positives, negatives, model):
+        output = output.unsqueeze(dim=1)
+        p_samples = positives.view(-1, *positives.size()[2:])
+        n_samples = negatives.view(-1, *negatives.size()[2:])
+        p_out = model(p_samples)
+        n_out = model(n_samples)
+        p_out = p_out.view(output.size(0), -1, p_out.size(-1))
+        n_out = n_out.view(output.size(0), -1, n_out.size(-1))
+        p_dist = (output * p_out).sum(dim=-1)
+        n_dist = (output * n_out).sum(dim=-1)
+        loss = -(F.log_softmax(torch.cat([p_dist, n_dist], dim=-1), dim=-1))[:, 0]
+        return loss.mean()
+
+
+class RecallMeter(meter.Meter):
+    def __init__(self, topk=[1]):
+        super(RecallMeter, self).__init__()
+        self.topk = topk
+        self.reset()
+
+    def reset(self):
+        self.sum = {v: 0 for v in self.topk}
+        self.n = 0
+
+    def add(self, output, index, label, database, database_labels):
+        no = output.size(0)
+        output, index, label = output.unsqueeze(dim=1), index.unsqueeze(dim=-1), label.unsqueeze(dim=-1)
+        database = database.unsqueeze(dim=0)
+
+        pred = torch.argsort((output * database).sum(dim=-1), descending=True)
+        # make sure it don't contain itself
+        pred = pred[pred != index].view(no, -1)
+        for k in self.topk:
+            recalled = pred[:, 0:k]
+            correct = (database_labels[recalled] == label).any(dim=-1)
+            self.sum[k] += correct.sum().item()
+        self.n += no
+
+    def value(self, k=-1):
+        if k != -1:
+            return float(self.sum[k]) / self.n * 100.0
+        else:
+            return [self.value(k_) for k_ in self.topk]
+
+
+class RetrievalDataset(Dataset):
+    def __init__(self, data_name, data_type='train', k=10):
+
+        if data_type == 'val':
+            data = read_json('data/{}/train_images.json'.format(data_name))
+        else:
+            data = read_json('data/{}/{}_images.json'.format(data_name, data_type))
+        self.transform = get_transform(data_name, data_type)
+        self.data_type, self.k = data_type, k
+        self.images, self.labels = list(data.keys()), list(data.values())
+        # make map between classes and labels
+        classes, labels = sorted(set(data.values())), {}
+        if self.k > len(classes) - 1:
+            raise IndexError('k must less than classes-1({})'.format(len(classes) - 1))
+        for index, label in enumerate(classes):
+            labels[label] = index
+        for index, label in enumerate(self.labels):
+            self.labels[index] = labels[label]
+        # self.indexes = set(range(len(self.images)))
+        self.dict_images = {}
+        for img, label in zip(self.images, self.labels):
+            if label in self.dict_images:
+                self.dict_images[label].append(img)
+            else:
+                self.dict_images[label] = [img]
+
+    def __getitem__(self, index):
+        img_path, label = self.images[index], self.labels[index]
+        img = self.transform(Image.open(img_path).convert('RGB'))
+
+        if self.data_type == 'train':
+            positive_database = self.dict_images[label]
+            # make sure the search database don't contain itself
+            positive_database = list(set(positive_database) - {img_path})
+            # choose 1 positive
+            positive_path = random.choice(positive_database)
+            positive_img = self.transform(Image.open(positive_path).convert('RGB')).unsqueeze(dim=0)
+            # select all negative samples
+            negative_database, negative_imgs = [], []
+            for database_label, database_images in self.dict_images.items():
+                if database_label != label:
+                    negative_database.append(database_images)
+            # choose k negative samples
+            negative_database = random.choices(negative_database, k=self.k)
+            for images in negative_database:
+                negative_path = random.choice(images)
+                negative_img = self.transform(Image.open(negative_path).convert('RGB'))
+                negative_imgs.append(negative_img)
+            negative_imgs = torch.stack(negative_imgs)
+            return img, positive_img, negative_imgs
+        else:
+            return img, label, index
+
+    def __len__(self):
+        return len(self.images)
