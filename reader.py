4e161e390b6ae8f1341103221614132f2861740b
leftthomas <leftthomas@qq.com>
2019-06-24 12:06:46 +0800

    simplified code

--- reader.py
+++ reader.py
@@ -1,9 +1,5 @@
-import torch.utils.data as data
-
 from PIL import Image
-from PIL import ImageFile
-
-ImageFile.LOAD_TRUNCATED_IMAGES = True
+from torch.utils.data import Dataset
 
 IMG_EXTENSIONS = [
     '.jpg', '.JPG', '.jpeg', '.JPEG',
@@ -40,42 +36,9 @@ def make_dataset(dict, class_to_idx):
     return images, intervals, idx_to_class
 
 
-def pil_loader(path):
-    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
-    with open(path, 'rb') as f:
-        with Image.open(f) as img:
-            return img.convert('RGB')
+class ImageReader(Dataset):
 
-
-def accimage_loader(path):
-    import accimage
-    try:
-        return accimage.Image(path)
-    except IOError:
-        # Potentially a decoding problem, fall back to PIL.Image
-        return pil_loader(path)
-
-
-def default_loader(path):
-    from torchvision import get_image_backend
-    if get_image_backend() == 'accimage':
-        return accimage_loader(path)
-    else:
-        return pil_loader(path)
-
-
-def label_transform():
-    pass
-
-
-def folderReader(path):
-    pass
-
-
-class ImageReader(data.Dataset):
-
-    def __init__(self, data_dict, transform=None, target_transform=None,
-                 loader=default_loader):
+    def __init__(self, data_dict, transform=None, target_transform=None):
 
         classes, class_to_idx = find_classes(data_dict)
         imgs, intervals, idx_to_class = make_dataset(data_dict, class_to_idx)
@@ -90,7 +53,6 @@ class ImageReader(data.Dataset):
         self.idx_to_class = idx_to_class  # i(img idx)->2(class)
         self.transform = transform
         self.target_transform = target_transform
-        self.loader = loader
 
     def __getitem__(self, index):
         """
@@ -100,7 +62,7 @@ class ImageReader(data.Dataset):
             tuple: (image, target) where target is class_index of the target class.
         """
         path, target = self.imgs[index]
-        img = self.loader(path)
+        img = Image.open(path).convert('RGB')
         if self.transform is not None:
             img = self.transform(img)
         if self.target_transform is not None:
